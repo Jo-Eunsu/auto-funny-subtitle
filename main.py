da8e135e9acf6f2f1791585789d479bfd42583ee
Jo-Eunsu <in2theblusky@gmail.com>
2021-03-30 16:37:57 +0900

    xml 불러오기 기능 추가
    Former-commit-id: af32325596faecaf3ad3b1cc405e6f8b12f6bd98

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 
 from xml.etree.ElementTree import parse
 
 
-# Azure AI Text Cognitive 서비스에 연결
+# Azure AI Text Analytics 서비스에 연결
 key = "2260af3f04d142f58f4597d6db740ac7"
 endpoint = "https://chosun-capstone-cognitive.cognitiveservices.azure.com/"
 def authenticate_client():
@@ -15,19 +16,11 @@ def authenticate_client():
     return text_analytics_client
 client = authenticate_client()
 
-
-# Import & Parse XML File
-video_info = parse('video-sample/히밥님...재료가 다 떨어졌어요...-TTfzRbSFtLY.fcpxml')
-root = video_info.getroot()
-
-for title in root.iter("title"):
+# Azure AI Text Analytics 서비스(client)를 이용해 
+# 텍스트(documents)의 감정(긍정, 중립, 부정) 분석
+def sentiment_analysis(client, documents):
     
-
-
-def sentiment_analysis_example(client):
-    
-    documents = ["모든 게 여유로워 보이는 이른 시간"]
-    response = client.analyze_sentiment(documents=documents, language="ko")[0]
+    response = client.analyze_sentiment(documents=documents, language="ko")
     print("Document Sentiment: {}".format(response.sentiment))
     print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
         response.confidence_scores.positive,
@@ -43,4 +36,11 @@ def sentiment_analysis_example(client):
             sentence.confidence_scores.negative,
         ))
           
-sentiment_analysis_example(client)
+sentiment_analysis(client, "모든 게 여유로워 보이는 이른 시간")
+
+# Import & Parse XML File
+video_info = parse('video-sample/히밥님...재료가 다 떨어졌어요...-TTfzRbSFtLY.fcpxml')
+root = video_info.getroot()
+
+for title in root.iter("title"):
+    print(title.attrib["name"], "- 스타일: ", title.attrib["ref"])
