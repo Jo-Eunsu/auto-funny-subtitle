7be21f8f10a28971a8b5c216914905a9d14fe903
Jo-Eunsu <in2theblusky@gmail.com>
2021-03-30 15:50:07 +0900

    예시 동영상 파일과 xml 파일 업로드
    Former-commit-id: 825b97e1fa5e7fb773416f16b4ae169d392346ed

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 
+from xml.etree.ElementTree import parse
+
+
+# Azure AI Text Cognitive 서비스에 연결
 key = "2260af3f04d142f58f4597d6db740ac7"
 endpoint = "https://chosun-capstone-cognitive.cognitiveservices.azure.com/"
-
 def authenticate_client():
     ta_credential = AzureKeyCredential(key)
     text_analytics_client = TextAnalyticsClient(
             endpoint=endpoint, 
             credential=ta_credential)
     return text_analytics_client
-
 client = authenticate_client()
+
+
+# Import & Parse XML File
+video_info = parse('video-sample/히밥님...재료가 다 떨어졌어요...-TTfzRbSFtLY.fcpxml')
+root = video_info.getroot()
+
+for title in root.iter("title"):
+    
+
 
 def sentiment_analysis_example(client):
     
-    documents = ["이러는데 쟤들이 안 미쳤다고?"]
+    documents = ["모든 게 여유로워 보이는 이른 시간"]
     response = client.analyze_sentiment(documents=documents, language="ko")[0]
     print("Document Sentiment: {}".format(response.sentiment))
     print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
