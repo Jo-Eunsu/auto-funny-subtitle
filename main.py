59ade2d54e860b387de7d0d97ede521cc5f462b1
Jo Eunsu <in2theblusky@gmail.com>
2021-04-05 13:43:49 +0900

    샘플 소스코드 주석 추가

--- main.py
+++ main.py
@@ -14,8 +14,11 @@ def authenticate_client():
 client = authenticate_client()
 
 def sentiment_analysis_example(client):
-    
-    documents = ["이러는데 쟤들이 안 미쳤다고?"]
+    # 1. documents 내용물에 텍스트를 입력해서 결과를 확인한다.
+    # 2. 결과를 스크린샷으로 찍는다. (10개 정도)
+    # 3. 스크린샷을 sample_screenshots 폴더에 올린다.
+    # 4. 스크린샷을 보고 어떻게 정확도를 올릴까 분석해본다.
+    documents = ["여기에 텍스트 입력"]
     response = client.analyze_sentiment(documents=documents, language="ko")[0]
     print("Document Sentiment: {}".format(response.sentiment))
     print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
