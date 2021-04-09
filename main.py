e0e64ecf06c2368da835b2dcb1ad944361f5b875
kimjinhwan <wlsghks4620@naver.com>
2021-04-09 16:28:41 +0900

    주석을 조금 달아봄 ㅎㅎ

--- main.py
+++ main.py
@@ -5,33 +5,33 @@ EMPTY = -1
 
 POSITIVE = 1
 NEUTRAL = 0
-NEGATIVE = -1
+NEGATIVE = -1 # 프로그램 작성 하기 쉽게 매크로 변수지정
 
-key = "2260af3f04d142f58f4597d6db740ac7"
-endpoint = "https://chosun-capstone-cognitive.cognitiveservices.azure.com/"
+key = "2260af3f04d142f58f4597d6db740ac7"   #애저의 텍스트 애널리틱스 라는 놈의 api에 접속하기 위한 키
+endpoint = "https://chosun-capstone-cognitive.cognitiveservices.azure.com/"   #접속하기 위한 엔드포인트(있어야 api에 접속할수있다)
 
-def authenticate_client():
-    ta_credential = AzureKeyCredential(key)
+def authenticate_client():   #api에 연결시키는 함수로서 이미 정의 된 함수를 가져와 사용 이름을 바꿀수 없음
+    ta_credential = AzureKeyCredential(key)  
     text_analytics_client = TextAnalyticsClient(
             endpoint=endpoint, 
             credential=ta_credential)
     return text_analytics_client
 
-client = authenticate_client()
+client = authenticate_client()  #client는 api연결이 성공하면 client변수에다가 api에 접속하기 위한 값을 대입 (api에 접속하는 클라이언트 변수 선언)
 
-def detect_abuse(sentence: str) -> int:
+def detect_abuse(sentence: str) -> int: #욕설 걸러주는 함수인데 sentence(인자값)를 사용해서 문자열로 받아낸다. return값이 int임을 나타냄 
     filter = ["시발", "개새끼", "병신", "썅", "좆같네", "좃같네", "좋같네", "아 씨", "아씨", "야발"]
-    for filter_item in filter:
-        if sentence.find(filter_item) is not EMPTY:
-            return sentence.find(filter_item)
-    return EMPTY
+    for filter_item in filter: #item은 하나씩 골라서 index로 저장시키는 함수이다.
+        if sentence.find(filter_item) is not EMPTY: #filter_item의 숫자를 돌려 받아낸후 비어있는지 확인한다
+            return sentence.find(filter_item)  #비어있는게 아니니까 -1대신 원래 기존 값을 리턴한다
+    return EMPTY  # 포함되는게 없는 상태이므로 -1을 반환한다.
 
 def detect_interjection(sentence: str) -> int:
     filter = ["무야호", "오진다", "오매", "좋은그" ]
     for filter_item in filter:
         if sentence.find(filter_item) is not EMPTY:
             return sentence.find(filter_item)
-    return POSITIVE
+    return EMPTY
 
 
 def sentiment_analysis_example(client):
@@ -43,7 +43,7 @@ def sentiment_analysis_example(client):
     # 2. 결과를 스크린샷으로 찍는다. (10개 정도)
     # 3. 스크린샷을 sample_screenshots 폴더에 올린다.
     # 4. 스크린샷을 보고 어떻게 정확도를 올릴까 분석해본다.
-    documents = ["무야호 시발"]
+    documents = ["야발 무야호"]
     
     # 미리 걸러낼 것들이 있으면 if문으로 걸러내기
     if documents[0].find("ㅋㅋㅋㅋㅋ") is not EMPTY:
